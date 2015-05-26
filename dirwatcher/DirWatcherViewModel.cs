88bc914c2723b32c65c23afca1ac9716acd179b3
DLed <dmlled@yahoo.com>
2015-05-26 10:15:48 +0200

    catch exceptions

--- dirwatcher/DirWatcherViewModel.cs
+++ dirwatcher/DirWatcherViewModel.cs
@@ -43,19 +43,28 @@ namespace dirwatcher
             var changed = ToTick(watcher.Changed,"U");
             var created = ToTick(watcher.Created,"C");
             var deleted = ToTick(watcher.Deleted,"D");
-                
-            changed.Merge(created.Merge(deleted))
+
+            var merged = changed.Merge(created.Merge(deleted));
+
+            var merged_with_exceptions = merged.Catch<Tick, Exception>(ex => Observable.Return<Tick>(new Tick
+            {
+                FullPath = ex.ToString(),
+                Type = "[Error]"
+            })
+            .Merge(merged));
+
+            merged_with_exceptions
             .Select(f =>
                     String.Format("{0}[{1}]: {2}{3}",
                         DateTime.Now.ToString("hh:mm:ss.fff"),
                         f.Type,
                         File.Exists(f.FullPath) ? "[F]" : "",
                         f.FullPath)
-                )
-                .Scan(new StringBuilder(),(b,f)=>b.Insert(0,String.Format("{0}\n",f)))
-                .Select(b=>b.ToString())
-                .ToProperty(this,vm=>vm.Log,out _Log)
-                ;
+            )
+            .Scan(new StringBuilder(),(b,f)=>b.Insert(0,String.Format("{0}\n",f)))
+            .Select(b=>b.ToString())
+            .ToProperty(this,vm=>vm.Log,out _Log)
+            ;
         }
     }
 }
